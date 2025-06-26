779744a2be4011fc440a66222926d144ed7d5274
devendra-70 <devendramahajan70@gmail.com>
2025-06-26 14:16:01 +0530

    added kafka in container, added protobut for patient topic producer

---
+++ patient-service/src/main/java/com/pm/patient_service/kafka/KafkaProducer.java
@@ -0,0 +1,16 @@
+package com.pm.patient_service.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public kafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate){
+        this.kafkaTemplate=kafkaTemplate;
+    }
+
+
+}
