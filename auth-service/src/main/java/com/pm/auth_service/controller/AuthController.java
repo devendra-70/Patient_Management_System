4f8662063d906abca2b37727569db374e7873130
devendra-70 <devendramahajan70@gmail.com>
2025-07-01 16:13:18 +0530

    created loggers for jwt token

--- auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.pm.auth_service.dto.LoginRequestDTO;
 import com.pm.auth_service.dto.LoginResponseDTO;
 import com.pm.auth_service.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +18,7 @@ import java.util.Optional;
 @RestController
 public class AuthController {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
     private final AuthService authService;
 
     public AuthController(AuthService authService){
@@ -33,6 +36,7 @@ public class AuthController {
         }
 
         String token = tokenOptional.get();
+        log.info("Generated token:{}",token);
         return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
